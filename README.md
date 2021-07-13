bb88325d7289015de0509368366dd6b84a8312b0
Anton Voronov <avoronov@at-consulting.ru>
2021-07-13 12:25:01 +0300

    Через Терраформ создали бакет в котором будем хранить статус (бэкенд)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # microserviceotus
+
+1. Создаем инфраструктуру
+
+1.1. Terraform
+1.1.1. Создаем бакет в облаке, где будем хранить статус нашей инфраструктуры ./terraform/backet/
+    - cd ./terraform/backet
+    - terraform init
+    - terraform apply -var-file terraform.tfvars
+    на выходе получаем ./terraform/state.tf - бэкенд с хранением терраформ стейта в созданам бакете в облаке
+1.1.2. Создаем создаем
---
+++ infra/terraform/backet/outputs.tf
@@ -0,0 +1,23 @@
+# имя бакета в облаке
+output "bucket_name" {
+  value = yandex_storage_bucket.infrastructure.bucket
+}
+# айди ключа доступа
+output "bucket_access_key" {
+  value = yandex_storage_bucket.infrastructure.access_key
+}
+# секретный ключ доступа
+output "bucket_secret_key" {
+  value = yandex_storage_bucket.infrastructure.secret_key
+}
+
+resource "local_file" "backend_bucket" {
+  content = templatefile("state.tf.tmpl",
+    {
+      name = yandex_storage_bucket.infrastructure.bucket,
+      access_key  = yandex_storage_bucket.infrastructure.access_key,
+      secret_key = yandex_storage_bucket.infrastructure.secret_key
+    }
+  )
+  filename = "../state.tf"
+}
